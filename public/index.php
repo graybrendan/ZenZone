bb86763c30aeda0367e4f0d8b589c63272961fe3
Brendan Gray <Brendan.gray@vikings.berry.edu>
2026-04-20 07:43:09 -0400

    modified:   public/index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,9 @@
-<?php ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ZenZone</title>
-</head>
-<body>
-    <h1>ZenZone is running</h1>
-    <p>Your XAMPP project folder is set up correctly.</p>
-</body>
-</html>
+<?php
+require_once __DIR__ . '/../includes/config.php';
+require_once __DIR__ . '/../includes/session.php';
+
+if (isLoggedIn()) {
+    authRedirect('dashboard.php');
+}
+
+authRedirect('login.php');
